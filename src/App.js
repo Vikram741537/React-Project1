56a9147c5aa67c15c4ce2c849975574535b4d7a3
Vikram <kushwahvikramsingh741@gmail.com>
2024-08-05 18:36:33 +0530

    first  commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, {useState} from "react";
+import Expenses from "./components/Expenses/Expenses.js";
+import NewExpense from "./components/NewExpense/NewExpense.js";
+let DUMMY_EXPENSE = [
+    {
+        id:'e1',
+        title:'School Fee',
+        amount:'250',
+        date: new Date(2021, 5, 12)
+    },
+    {
+        id:'e2',
+        title:'Grocery',
+        amount:'150',
+        date: new Date(2021, 3, 12)
+    },
+    {
+        id:'e3',
+        title:'Books',
+        amount:'230',
+        date: new Date(2021, 2, 12)
+    },
+    {
+        id:'e4',
+        title:'House Rent',
+        amount:'700',
+        date: new Date(2021, 6, 12)
+    }
+]
+const App = () => {
+    const [expenses, setExpenses] = useState(DUMMY_EXPENSE)
+    const addExpenseHandler = (expense) =>{
+        const updatedExpense = [expense, ...expenses]
+      setExpenses(updatedExpense)
+    }
+    return (
+        <div>
+            <NewExpense onAddExpense = {addExpenseHandler} />
+            <Expenses item = {expenses}/>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/Users.js
@@ -0,0 +1,27 @@
+// import React,{Component} from "react";
+//class component 
+
+//  class Users extends Component
+// {
+//     render(){
+//         console.log("render 1");
+//         return(
+//             <h1>Hello from User</h1>
+
+//         )
+//     }
+// }
+
+//functional component
+// function Users() {
+//     return (
+//        <div><h1>Users Component</h1>
+//         <h1>hello</h1>
+//         </div> 
+//     );
+//   }
+// export default Users;
+
+
+
+
